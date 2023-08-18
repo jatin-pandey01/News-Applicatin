06c7652d6cc9b1e821db41080bb77bb260485228
jatin-pandey01 <=>
2023-08-18 16:06:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+const apiKey = 'e1b060327fa8467997a5dbc212e9112f';
+const optionsContainer = document.querySelector('.options-container');
+const newsContainer = document.querySelector('.news-container');
+const country = 'in';
+const options = ['general','entertainment','health','science','sports','technology'];
+var requestURL;
+
+function generateUI(articles){
+    for(let item of articles){
+        const card = document.createElement('div');
+        card.classList.add('news-card');
+        card.innerHTML = `<div class = "news-image-container"> 
+        <img src = "${item?.urlToImage || "./images/newsPaper.jpeg"}" alt="" />
+        <div class = "news-content">
+            <div class = "news-title">
+                ${item.title}
+            </div>
+            <div class = "news-desc">
+                ${item.description || item.content || ""}
+            </div>
+            <a href="${item.url}" target="_blank" class = "view-button">Read More</a>
+        </div>`;
+        newsContainer.appendChild(card);
+    }
+}
+
+async function getNews(requestURL){
+    newsContainer.innerHTML = "";
+    const response = await fetch(requestURL);
+    console.log(response);
+    const data = await response.json();
+    console.log(response.ok);
+    if(!response.ok){
+        alert('Data unavailbale at the moment. Please try again later.');
+        return false;
+        // console.log(response.ok);
+    } 
+    generateUI(data.articles);
+    console.log(data);
+            
+}
+
+function selectCategory(e,category){
+    let options = document.querySelectorAll('.options');
+    options.forEach((element)=>{
+        element.classList.remove('active');
+    });
+    requestURL = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`;
+    e.target.classList.add('active');
+    getNews(requestURL);
+}
+
+function createOptions(){
+    console.log('Function');
+    for(let i of options){
+        console.log(`${i}`);
+        optionsContainer.innerHTML += `<button class='options ${i == "general" ? "active":""}'
+        onclick = "selectCategory(event,'${i}')">${i}</button>`;
+    }
+}
+
+const init = ()=>{
+    optionsContainer.innerHTML ="";
+    getNews(requestURL);
+    createOptions();
+}
+
+window.onload = ()=>{
+    requestURL = `https://newsapi.org/v2/top-headlines?country=${country}&category=general&apiKey=${apiKey}`;
+    init();
+    return;
+};
